a2b535f71ee0e7f96f03c6a9b584c4d58ede3e55
ar9entummm <pakholakdima222@icloud.com>
2025-02-08 19:00:50 -0500

    Init commit

---
+++ AstroBot/Program.cs
@@ -0,0 +1,7 @@
+﻿// Developer: ar9entum
+// Discord bot Memories shop created in C# using Discord.net library
+// Дискорд бот Memories shop созданный на языке C# с использованием библиотеки Discord.net
+
+// Main file
+// Основной файл
+
